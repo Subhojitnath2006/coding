db89c7a96fae997071191ecbb45c0f8ebc0c8176
Subhojit Nath <rabinath750@gmail.com>
2024-02-27 07:33:28 +0530

    sum

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf(" enter the value of a is");
+    scanf("%d",&a);
+    int b;
+    printf(" enter the value of b is");
+    scanf("%d",&b);
+    printf("the sum is: %d \n",a+b);
+    return 0;
+}
