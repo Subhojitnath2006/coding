b097885c5a778f2ca599f577d5c5a0472429ce54
Subhojit Nath <rabinath750@gmail.com>
2024-02-27 07:00:54 +0530

    first commit

---
+++ syntax.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int number = 25;
+    char star = '*';
+    int age = 22;
+    age = 24; // we can change the variable as per our own requirements.
+    float pi = 3.14;
+
+    int a = 30;
+    int A = 40; // here a and A is different variable.
+
+    //    int _age= 22;
+
+    int final_price = 100; // we can only use _ in variable name.
+}
