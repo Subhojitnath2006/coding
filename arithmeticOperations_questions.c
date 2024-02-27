c80ff2df25af8e98ce1f96956702a5ed17b8182c
Subhojit Nath <rabinath750@gmail.com>
2024-02-27 09:26:03 +0530

    arithmetic questions

---
+++ arithmeticOperations_questions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+
+    int a= 5*2-2*3;
+    printf(" The value of a is: %d \n",a);
+
+
+    int b= 5*2/2*3;
+    printf(" The value of b is: %d \n",b);
+
+    int c= 5*(2/2)*3;
+    printf(" The value of c is: %d \n",c);
+    
+    int d= 5+2/2*3;
+    printf(" The value of d is: %d \n",d);
+    return 0;
+}
