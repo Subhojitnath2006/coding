4874c32eb76d16e192c1b91cdb58946e36808e08
Subhojit Nath <rabinath750@gmail.com>
2024-02-27 20:37:29 +0530

    conditional operators

---
+++ control_instructions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+
+    //printf("%d \n",4==4);  true-->1  false---->0
+    // printf("%d \n",4>4);
+    // printf("%d \n",4>3);
+    // printf("%d \n",3>4);
+
+    // printf("%d \n",4!=4);
+    // printf("%d \n",4==4);
+    return 0;
+}
