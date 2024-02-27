5a82f244b431fdd378ff7ee95623a78443bec8db
Subhojit Nath <rabinath750@gmail.com>
2024-02-27 07:29:48 +0530

    a

--- datatypes.c
+++ datatypes.c
@@ -1,7 +1,20 @@
-# include <stdio.h>
-int main(){
-    int age=22;
-    float pi= 3.14;
-    char hastag= '#';
+#include <stdio.h>
+int main()
+{
+    int Age = 22;
+    float pi = 3.14;
+    char hastag = '#';
+
+    // printf("age is %d \n",age);
+    // printf("value of pi is %f \n",pi);
+    // printf("hastage %c \n",hastag);
+    // printf() is used to give output in c
+
+    // scanf() is used to take input in c
+    int age;
+    printf("age is");
+    scanf("%d", &age);
+    printf("age is : %d \n", age);
+
     return 0;
 }
