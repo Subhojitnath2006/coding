77cb02d519facb86973394f17fd1c05a2c996ede
Subhojit Nath <rabinath750@gmail.com>
2024-02-28 21:37:43 +0530

    functions

---
+++ function_and_recursion/functions.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void printHello(); //function prototype/declaration
+int main(){
+
+    printHello();  //function call
+    return 0;
+}
+
+void printHello(){
+    printf("hello world"); //function definition
+}
