1457b5c08c4dbf48bc0f67757d375a009783afa4
Subhojit Nath <rabinath750@gmail.com>
2024-03-03 18:56:52 +0530

    pointer

--- pointers/pointerr.c
+++ pointers/pointerr.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
-int main(){
+int main()
+{
 
-    int a=5;
-    int* x=&a;  //it stores the address of a 
-    printf("%p \n",&a);  // prints the address of a.
-    printf("%p \n",&x);  //%p prints the address 
-    printf("%p \n",x);  // prints the address of a
-    printf("%p \n",*x);  //prints the value corresponding to the address stored in x.
+    int a = 5;
+    int *x = &a;         // it stores the address of a
+    printf("%p \n", &a); // prints the address of a.
+    printf("%p \n", &x); //%p prints the address
+    printf("%p \n", x);  // prints the address of a
+    printf("%p \n", *x); // prints the value corresponding to the address stored in x.
+
+    int **y = &x;  //int* ---> int ka address store karta ha.
+    // int** ---> int* ka address store karta ha.
+    printf("%p \n", y);
+    printf("%d \n", **y);
     return 0;
 }
