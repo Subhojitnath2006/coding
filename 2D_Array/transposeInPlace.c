6ac0194069d3b5827ce3ecd0b36b84b07b831d51
Subhojit Nath <rabinath750@gmail.com>
2024-03-09 19:48:28 +0530

    2d array

--- 2D_Array/transposeInPlace.c
+++ 2D_Array/transposeInPlace.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     int n;
     printf("the no of rows/columns:");
-    scanf("%d",&n);
-  
+    scanf("%d", &n);
+
     int arr[n][n];
-    
-   printf("enter all the elements:\n");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            scanf("%d ",arr[i][j]);
+
+    printf("enter all the elements:\n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d ", arr[i][j]);
         }
     }
     printf("\n");
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            //swap arr[i][j] and arr[j][i]
-            int temp=arr[i][j];
-            arr[i][j]=arr[j][i];
-            arr[j][i]=temp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            // swap arr[i][j] and arr[j][i]
+            int temp = arr[i][j];
+            arr[i][j] = arr[j][i];
+            arr[j][i] = temp;
         }
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            printf("%d ",arr[i][j]);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%d ", arr[i][j]);
         }
         printf("\n");
     }
-  
+
     return 0;
 }
