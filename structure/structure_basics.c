fdc1122ecd2704dd6f61eae08341ca006cfb25bd
Subhojit Nath <rabinath750@gmail.com>
2024-03-29 08:00:11 +0530

    structure basics

---
+++ structure/structure_basics.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+
+    // structures--> user defined data types
+    //syntax
+    struct pokemon
+    {
+        int hp;
+        int speed;
+        int attack;
+        char tier; //S,A,B,C
+    };
+    struct pokemon pikachu;
+
+    printf("enter attack of pikachu");
+    scanf("%d",pikachu.attack);
+    //pikachu.attack = 60;
+    pikachu.hp = 50;
+    pikachu.speed = 100;
+    pikachu.tier= 'A';
+
+    printf("%d",pikachu.attack);
+
+    struct pokemon charizard;
+    charizard.attack = 130;
+    charizard.hp = 80;
+    charizard.speed = 80;
+    charizard.tier='S';
+    return 0;
+}
