8c114b8e3ec98991e228bb91d0183df52bbc89d4
Subhojit Nath <rabinath750@gmail.com>
2024-03-31 07:39:38 +0530

    structure

---
+++ structure/basic_questions.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+
+    // struct book{
+    //     char name[50];
+    //     float price;
+    //     int noOfPages;
+        
+    // }a,b,c;
+
+    // a.noOfPages= 100;
+    // a.price= 411.5;
+    // strcpy(a.name,"Secret Seven");
+
+    // printf("%d\n",a.noOfPages);
+    // printf("%f\n",a.price);
+    // printf("%s\n",a.name);
+
+    // struct Person{
+    //     char name[50];
+    //     int salary;
+    //     int age;
+    // };
+    //how are structure elements stored?
+
+    //elements are stored in a continuous memory location
+    
+    return 0;
+}
