3d50e48c91499cf38a0a4923b3383a939a0ef0e2
Subhojit Nath <rabinath750@gmail.com>
2024-04-01 07:57:34 +0530

    structures

--- structure/nesting.c
+++ structure/nesting.c
@@ -12,5 +12,29 @@ int main()
         char name[15];
 
     } pokemon;
+
+    // nesting one structure into another
+    typedef struct legendarypokemon
+    {
+        pokemon normal;
+        char ability[100];
+    } legendarypokemon;
+
+    typedef struct godpokemon
+    {
+        legendarypokemon legend;
+        int specialattack;
+    } godpokemon;
+
+    legendarypokemon mewtwo;
+    strcpy(mewtwo.ability, "presurre");
+    mewtwo.normal.hp = 150;
+    strcpy(mewtwo.normal.name, "Mewtwo");
+
+    godpokemon arceus;
+    arceus.specialattack = 300;
+    strcpy(arceus.legend.ability, "Turn anyone into stone");
+    arceus.legend.normal.hp = 150;
+
     return 0;
 }
